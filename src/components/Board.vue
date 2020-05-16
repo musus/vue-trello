cf03ba961bed80b487873bf98ef9cee5d9f3c6d5
Susumu Seino <seino@susu.mu>
2020-05-16 22:47:23 +0700

    Add ListAdd.vue

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -8,13 +8,18 @@
 		<p class="info-line">
 			すべてのタスク: 0
 		</p>
+		<list-add />
 	</main>
 </div>
 
 </template>
 
 <script>
+	import ListAdd from './ListAdd.vue'
 
 export default {
+	components: {
+		ListAdd
+	}
 }
 </script>
---
+++ src/components/ListAdd.vue
@@ -0,0 +1,26 @@
+<template>
+	<form class="addlist" @submit.prevent="addList">
+		<input type="text"
+		v-model="text"
+		class="text-input"
+		placeholder="リストを追加"
+		>
+		<button type="submit" class="add-button">追加</button>
+	</form>
+</template>
+
+<script>
+	export default {
+		data: function() {
+			return {
+				title: '',
+			}
+		},
+		methods: {
+			addList: function() {
+				this.$store.dispatch('addList', { title: this.title})
+				this.title = ''
+			}
+		}
+	}
+</script>
