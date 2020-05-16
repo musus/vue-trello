0c2d9a35336d4361c83bbb09003086c00201b234
Susumu Seino <seino@susu.mu>
2020-05-16 22:32:39 +0700

    Add board

---
+++ src/components/Board.vue
@@ -0,0 +1,20 @@
+<template>
+
+<div>
+	<header>
+		Task Checker
+	</header>
+	<main>
+		<p class="info-line">
+			すべてのタスク: 0
+		</p>
+	</main>
+</div>
+
+</template>
+
+<script>
+
+export default {
+}
+</script>
