12ccb1f5b11efed794ffc5daa635ff095cfdc69a
Susumu Seino <seino@susu.mu>
2020-05-17 09:16:16 +0700

    Add list data to store

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,19 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+  	lists: [],
   },
   mutations: {
+  	addList(state, payload) {
+  		state.lists.push({ title: payload.title, cards:[]})
+  	},
   },
   actions: {
+  	addlist(context, payload) {
+  		context.commit('addlist',payload)
+
+  	},
   },
-  modules: {
+  getters: {
   }
 })
