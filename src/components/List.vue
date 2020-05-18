6a9f1ce14bdb95d113d209a3420ac07a545ab0d3
Susumu Seino <seino@susu.mu>
2020-05-18 20:31:24 +0700

    Show card count in list

--- src/components/List.vue
+++ src/components/List.vue
@@ -2,6 +2,7 @@
 	<div class="list">
 		<div class="listheader">
 			<p class="list-title">{{ title }}</p>
+			<p class="list-counter">合計: {{ totalCardInList }}</p>
 			<div class="deletelist" @click="removeList">x</div>
 		</div>
 		<card v-for="(item, index) in cards"
@@ -37,6 +38,12 @@
 				type: Number,
 				required: true
 			}
+		},
+		computed: {
+			totalCardInList() {
+				return this.cards.length
+			}
+
 		},
 		methods: {
 			removeList: function() {
